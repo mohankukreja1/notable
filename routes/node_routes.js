9974f0cbd664b999817c2893c729629f07a58dc1
mohan <mohankukreja1@gmail.com>
2018-04-04 19:39:29 +0530

    read and delete operations
    Signed-off-by: mohan <mohankukreja1@gmail.com>

--- routes/node_routes.js
+++ routes/node_routes.js
@@ -1,4 +1,35 @@
+var ObjectID =require('mongodb').ObjectID;
+
 module.exports = function (app,db) {
+    app.get('/notes/:id',(req,res)=>{
+        var id = req.params.id;
+        const details= {'_id':new ObjectID(id)};
+        db.collection('notes').findOne(details,(err,item)=>{
+            if(err){
+                console.log(err)
+            }
+            else{
+                res.send(item);
+
+            }
+        })
+    })
+
+
+    app.delete('/notes/:id',(req,res)=>{
+        var id = req.params.id;
+        const details= {'_id':new ObjectID(id)};
+        db.collection('notes').removeOne(details,(err,item)=>{
+            if(err){
+                console.log(err)
+            }
+            else{
+                res.send('item '+ id + ' deleted');
+
+            }
+        })
+    })
+
     app.post('/notes',(req,res)=>{
         const note={ text : req.body.body, title:req.body.title};
         db.collection('notes').insertOne(note , (err , result)=>{
